422734dfbe94c2482c55c0e4e2e69c0d15dcc051
racha <radjracha199@gmail.com>
2025-04-15 22:47:04 +0200

    initial commit

--- puzzle/src/application/Controler.java
+++ puzzle/src/application/Controler.java
@@ -1,7 +1,19 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class Controler {
-
+      
+	@FXML private Button btn1;
+	@FXML private Button btn2;
+	@FXML private Button btn3;
+	@FXML private Button btn4;
+	@FXML private Button btn5;
+	@FXML private Button btn6;
+	@FXML private Button btn7;
+	@FXML private Button btn8;
+	@FXML private Button btn9;
+	
+	
 }
