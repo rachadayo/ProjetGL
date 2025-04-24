776c82605c6b9b3560967da766ef20ec6623d1a9
racha <radjracha199@gmail.com>
2025-04-24 23:19:24 +0200

    new

--- puzzle/src/application/Main.java
+++ puzzle/src/application/Main.java
@@ -8,7 +8,7 @@ import javafx.scene.*;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.animation.*;
+
 
 
 public class Main extends Application {
