34c91176e26710045a21bcc73fcb92da7d6ce3a3
Naila <boukazianaila0@gmail.com>
2025-04-25 09:37:17 +0100

    new

--- puzzle/src/application/Main.java
+++ puzzle/src/application/Main.java
@@ -17,10 +17,12 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root, 600, 600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Slinding Puzzle");
+			primaryStage.setResizable(false);
+			primaryStage.centerOnScreen();
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
