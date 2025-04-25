72c49ddf399f059ff95f80f880b69422d5ae6786
racha <radjracha199@gmail.com>
2025-04-25 23:57:50 +0200

    new

--- puzzle/src/application/Main.java
+++ puzzle/src/application/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root, 600, 600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("bsmlhrhmanrahim");
+			primaryStage.setTitle("Puzzle Sliding");
 			primaryStage.setResizable(false);
 			primaryStage.centerOnScreen();
 			primaryStage.show();
